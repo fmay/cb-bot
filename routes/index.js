e467446a5ac3a6993b179019aba3b3e8b645b3ab
3bfred <3bfred@gmail.com>
2021-01-08 12:03:04 +0000

    pre auth mod

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const bent = require('bent')
 const querystring = require('querystring')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("GET: " + req.body.event)
@@ -16,14 +17,42 @@ router.post('/', function(req, res, next) {
       registerBot(req)
       break;
     case 'ONIMBOTMESSAGEADD':
-      sendMessage(req)
+      if(req.body.data.USER.IS_EXTRANET=='Y') {
+        //sendMessage(req)
+        sendKeyboard(req)
+      }
+      break;
+    case 'ONIMBOTJOINCHAT':
+        sendMessage(req)
       break;
     default:
       console.log(req.body)
   }
-  console.log()
+  console.log('---')
 });
 
+
+async function sendKeyboard(req) {
+  const data = {
+    //COMMAND_ID: '1',
+    MESSAGE_ID: req.body.data.PARAMS.MESSAGE_ID,
+    MESSAGE: 'Do you want to chat to us in person about CooklyBookly or do you want information on how to use CooklyBookly?',
+    KEYBOARD: [{
+      TEXT: 'I want to chat with someone',
+      BG_COLOR: '#62b2b3',
+      TEXT_COLOR: '#ffffff',
+      DISPLAY: 'LINE'
+    },
+    {
+      TEXT: 'I want to chat with someone',
+      BG_COLOR: '#62b2b3',
+      TEXT_COLOR: '#ffffff',
+      DISPLAY: 'LINE'
+    }],
+    'auth': req.body.auth.access_token
+  }
+  await callBitrix('imbot.command.answer', data, req.body.auth)
+}
 
 async function sendMessage(req) {
   const data = {
@@ -40,7 +69,7 @@ async function registerBot(req) {
   cbUrl = req.protocol + "://" + req.hostname + "/"
   data = {
     CODE: "CBBot-Beria",
-    TYPE: "O",
+    TYPE: "0",
     EVENT_MESSAGE_ADD: cbUrl,
     EVENT_WELCOME_MESSAGE: cbUrl,
     EVENT_BOT_DELETE: cbUrl,
@@ -58,11 +87,11 @@ async function callBitrix(bitrixMethod, post_data, auth) {
   const callPost = bent(`${auth.client_endpoint}`, 'POST', 'string', 200)
   try {
     const response = await callPost(`${bitrixMethod}`, querystring.encode(post_data))
-    console.log('response')
-    console.log(response)
+    //console.log('response')
+    //console.log(response)
 
   } catch(e) {
-    console.log(e)
+    console.log("Error : " + e)
   }
 
 }
