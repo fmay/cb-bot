6ca2124b67993be127af80d6cd26201d19546aac
3bfred <3bfred@gmail.com>
2021-01-09 13:10:49 +0000

    ready for keyboard attempt

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const bent = require('bent')
 const querystring = require('querystring')
 
+var _botId = ""
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("GET: " + req.body.event)
@@ -18,8 +20,8 @@ router.post('/', function(req, res, next) {
       break;
     case 'ONIMBOTMESSAGEADD':
       if(req.body.data.USER.IS_EXTRANET=='Y') {
-        sendMessage(req)
-        //sendKeyboard(req)
+        //sendMessage(req)
+        sendKeyboard(req)
       }
       break;
     case 'ONIMBOTJOINCHAT':
@@ -32,22 +34,26 @@ router.post('/', function(req, res, next) {
 
 async function sendKeyboard(req) {
   const data = {
-    COMMAND_ID: 'MainMenu',
+    //COMMAND_ID: 'MainMenu',
     //MESSAGE_ID: req.body.data.PARAMS.MESSAGE_ID,
     'DIALOG_ID': req.body.data['PARAMS']['DIALOG_ID'],
-    'MESSAGE': 'Do you want to chat to us in person about CooklyBookly or do you want information on how to use CooklyBookly?',
-    'KEYBOARD[0][TEXT]': 'I want to chat with someone',
-    'KEYBOARD[0][BG_COLOR]': '#62b2b3',
+    'SYSTEM': 'N',
+    'MESSAGE': 'Keyboard message',
+    'KEYBOARD[0][TEXT]': 'CHAT',
     'KEYBOARD[0][TEXT_COLOR]': '#ffffff',
+    'KEYBOARD[0][BG_COLOR]': '#62b2b3',
     'KEYBOARD[0][DISPLAY]': 'LINE',
-    'KEYBOARD[0][COMMAND]': 'main',
+    'KEYBOARD[0][HIDDEN]': 'N',
+    'KEYBOARD[0][LINK]': "https://google.com",
+    //'KEYBOARD[0][COMMAND]': 'MainMenu'
+    /*'KEYBOARD[0][COMMAND]': 'main',
     'KEYBOARD[0][COMMAND_PARAMS]': 'chat',
     'KEYBOARD[1][TEXT]': 'I have a product usage question',
     'KEYBOARD[1][BG_COLOR]': '#62b2b3',
     'KEYBOARD[1][TEXT_COLOR]': '#ffffff',
     'KEYBOARD[1][DISPLAY]': 'LINE',
     'KEYBOARD[1][COMMAND]': 'main',
-    'KEYBOARD[1][COMMAND_PARAMS]': 'support'
+    'KEYBOARD[1][COMMAND_PARAMS]': 'support'*/
   }
   await callBitrix('imbot.message.add', data, req.body.auth)
 }
@@ -67,7 +73,7 @@ async function sendMessage(req) {
     //'USER_TO_ID': req.body.data.PARAMS.TO_USER_ID,
     //'DIALOG_ID': 'chat'+ req.body.data.PARAMS.CHAT_ID,
     //'DIALOG_ID': req.body.data.USER.ID,
-    'USER_FROM_ID': req.body.data['PARAMS']['FROM_USER_ID'],
+    //'USER_FROM_ID': req.body.data['PARAMS']['FROM_USER_ID'],
     'DIALOG_ID': req.body.data['PARAMS']['DIALOG_ID'],
     'MESSAGE': `A simple message`
   }
@@ -78,7 +84,7 @@ async function sendMessage(req) {
 async function registerBot(req) {
   // Register Bot
   cbUrl = req.protocol + "://" + req.hostname + "/"
-  version = "CB Chatbot V2",
+  version = "CBChatbotV4"
   data = {
     CODE: version,
     TYPE: "O",
@@ -86,21 +92,31 @@ async function registerBot(req) {
     EVENT_HANDLER: cbUrl,
     'PROPERTIES[NAME]': version,
     'PROPERTIES[COLOR]': "AQUA",
-    'PROPERTIES[EMAIL]': "test@test.com",
+    'PROPERTIES[EMAIL]': "fmay@cooklybookly.com",
   }
   console.log("REGISTERING : " + data.CODE)
+
+  //await callBitrix('imbot.bot.list', {}, req.body.auth)
+  //delBots = []
+  //await callBitrix('imbot.unregister', {"BOT_ID": 107}, req.body.auth)
+  //return
+
   await callBitrix('imbot.register', data, req.body.auth)
 
+
   // Register Command
-  /*data = {
-    BOT_ID: 'CBBotX',
+  data = {
+    BOT_ID: _botId,
     COMMAND: "MainMenu",
     COMMON: 'N',
     HIDDEN: 'N',
     EXTRANET_SUPPORT: 'Y',
-    EVENT_COMMAND_ADD: cbUrl + "mainMenu"
+    'LANG[0][LANGUAGE_ID]': 0,
+    'LANG[0][TITLE]': "Get echo message",
+    'LANG[0][PARAMS]': "some text",
+    EVENT_COMMAND_ADD: cbUrl
   }
-  await callBitrix('imbot.command.register', data, req.body.auth)*/
+  await callBitrix('imbot.command.register', data, req.body.auth)
 }
 
 async function callBitrix(bitrixMethod, post_data, auth) {
@@ -110,6 +126,9 @@ async function callBitrix(bitrixMethod, post_data, auth) {
   const callPost = bent(`${auth.client_endpoint}`, 'POST', 'string', 200)
   try {
     const response = await callPost(`${bitrixMethod}`, querystring.encode(post_data))
+    if(bitrixMethod === "imbot.register") {
+      _botId = JSON.parse(response).result;
+    }
     console.log(response)
     console.log("-------------------")
 
